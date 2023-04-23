a6fbfaf7a836ba764314aa7a0800068717f48651
Antonio García <119652625+AntonioXDDD@users.noreply.github.com>
2023-04-23 15:42:22 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '735dce2b02c9431c639e10cf583412f8', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '14.670',
+	defaultLongitude: '-86.218',
 
 	// Autochange
 	autoChangeTheme: true,
